501f2e33a2edd1aeb4a0e490ff7c2d71c289c688
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2018-04-26 15:39:15 +0300

    Fixed a horribly broken demo

--- demo.py
+++ demo.py
@@ -6,8 +6,9 @@ import nslr_hmm
 t = np.arange(0, 5, 0.01)
 saw = ((t*10)%10)/10.0*10.0 # 10 deg/second sawtooth
 eye = np.vstack(( saw, -saw )).T
+eye += np.random.randn(*eye.shape)*0.1
 
-sample_class, segmentation, seg_class = nlsr_hmm.classify_gaze(t, eye)
+sample_class, segmentation, seg_class = nslr_hmm.classify_gaze(t, eye)
 
 COLORS = {
         nslr_hmm.FIXATION: 'blue',
@@ -16,11 +17,9 @@ COLORS = {
         nslr_hmm.PSO: 'yellow',
 }
 
-eye += np.random.randn(*eye.shape)*0.1
 plt.plot(t, eye[:,0], '.')
-
 for i, seg in enumerate(segmentation.segments):
     cls = seg_class[i]
     plt.plot(seg.t, np.array(seg.x)[:,0], color=COLORS[cls])
-    
+
 plt.show()
